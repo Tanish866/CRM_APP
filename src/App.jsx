1824690deddcb3af7a8e64ba9997a7bbb0b29cb2
Tanish Singh <tanishsingh866@gmail.com>
2026-03-19 16:35:15 +0000

    feat:Added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Login from './pages/Authentication/Login'
+import Signup from './pages/Authentication/Signup'
 
 function App() {
 
   return (
     <>
-      <Login/>
+      <Signup/>
     </>
   )
 }
--- src/pages/Authentication/Signup.jsx
+++ src/pages/Authentication/Signup.jsx
@@ -1,4 +1,4 @@
-function Login(){
+function Signup(){
     return (
         <div className="flex justify-center items-center h-[90vh]">
             <div className="card bg-neutral text-neutral-content w-96">
@@ -9,8 +9,8 @@ function Login(){
                             <path d="M4 20c0-4 3.6-7 8-7s8 3 8 7" stroke="#38c9f0" stroke-width="1.8"/>
                         </svg>
                     </div>
-                    <h2 class="text-2xl font-bold text-white tracking-wide">Welcome Back</h2>
-                    <p class="text-sm text-slate-400 mt-1">Login in to your account</p>
+                    <h2 class="text-2xl font-bold text-white tracking-wide">Create Account</h2>
+                    <p class="text-sm text-slate-400 mt-1">Sign up to get started</p>
                 </div>
                 <div className="card-body items-center text-start">
                     <div className="w-full">
@@ -20,10 +20,23 @@ function Login(){
                         <input type="text" placeholder="Enter the user id " className="input input-primary" />
                     </div>
                     <div className="w-full">
+                        <div>EMAIL</div>
+                    </div>
+                    <div className="w-full">
+                        <input type="email" placeholder="Enter the email" className="input input-primary" />
+                    </div>
+                    <div className="w-full">
                         <div>PASSWORD</div>
                     </div>
                     <div className="w-full">
                         <input type="password" placeholder="Enter the password" className="input input-primary" />
+                    </div>
+                    <div className="dropdown w-full">
+                        <div tabIndex={0} role="button" className="btn m-1">USER TYPE</div>
+                        <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-35 p-2 shadow-sm">
+                            <li><a>Customer</a></li>
+                            <li><a>Engineer</a></li>
+                        </ul>
                     </div>
                     <div className="w-full">
                         <button className="btn btn-warning  font-bold text-lg w-full mt-8">Submit</button>
@@ -31,6 +44,6 @@ function Login(){
                 </div>
             </div>
         </div>
-    );
+    ); 
 }
-export default Login;
+export default Signup;
